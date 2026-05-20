cd99b48b17418785a9ce0645c37489f1b4ecfe21
Kareemah-codes <krimmyninja@gmail.com>
2026-05-20 18:59:31 +0100

    Submit week 5 assigment

---
+++ week_5/leap_year.js
@@ -0,0 +1,30 @@
+// import readline module 
+
+import {createInterface} from "readline";
+
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Welcome to the leap year determiner \n Input a year : ', (year)=> {
+    year = Number(year)
+if (year % 100 == 0) {
+   if (year % 400 == 0){
+    console.log(29)
+   } else {
+    console.log(28)
+   }
+}
+else if (year% 4 ==0)
+    {
+        console.log(29)
+    }
+else{
+        console.log(28)
+    }
+
+    readline.close();
+});
+
+
