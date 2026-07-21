c6e4855af66c79aaea9d6d88a6954ee05faa9cc6
Kareemah-codes <krimmyninja@gmail.com>
2026-07-21 13:14:19 +0100

    Files moved and gitignore added

---
+++ week_14/auth-api/index.js
@@ -0,0 +1,17 @@
+// entry point, registers routes
+
+//Users can register,login and manage notes.
+// 1 user can have a set of notes that belong to that user only.
+
+/*
+Functions and enpoints
+
+/user/register -> POST REQUEST, enable users to register, validations must be put for user input
+/user/login -> POST REQUEST, user will input their username and password. 
+/user/note/read -> loads up a particular note associated with particular user
+
+/user/note/add -> enables user to create and add notes
+
+
+
+*/
