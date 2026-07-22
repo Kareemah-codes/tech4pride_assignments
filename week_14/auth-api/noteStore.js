98690a72559660719055513f6fd43f74a7395c04
Kareemah-codes <krimmyninja@gmail.com>
2026-07-22 18:38:40 +0100

    Template for File Helpers

--- week_14/auth-api/noteStore.js
+++ week_14/auth-api/noteStore.js
@@ -1,1 +1,31 @@
 //helper functions for reading/writing notes.json
+
+
+// USERS
+const fs = require('fs/promises')
+const path = 'notes.json';
+
+async function readNotes(){
+    try {
+      data = await fs.readFile(path,'utf-8');
+      return JSON.parse(data)
+    }
+    catch (err){
+        console.log(`Error ... ${err}`)
+    }
+}
+
+async function writeNotes(notes){
+    try{
+        const newUser = '';
+        await fs.writeFile(path,content)
+    } catch(err){
+        console.log(`Error ... ${err}`)
+    }
+}
+
+async function main(){
+    const notes = await readNotes();
+    
+}
+main();
--- week_14/auth-api/userStore.js
+++ week_14/auth-api/userStore.js
@@ -1,1 +1,28 @@
-//helper functions for reading/writing users.json
+// USERS
+const fs = require('fs/promises')
+const path = 'users.json';
+
+async function readUsers(){
+    try {
+      data = await fs.readFile(path,'utf-8');
+      return JSON.parse(data)
+    }
+    catch (err){
+        console.log(`Error ... ${err}`)
+    }
+}
+
+async function writeUsers(users){
+    try{
+        const newUser = '';
+        await fs.writeFile(path,content)
+    } catch(err){
+        console.log(`Error ... ${err}`)
+    }
+}
+
+async function main(){
+    const users = await readUsers();
+    
+}
+main();
