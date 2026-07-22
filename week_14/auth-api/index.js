4f62f7a198066020b53f44594d497831628760a9
Kareemah-codes <krimmyninja@gmail.com>
2026-07-22 15:19:29 +0100

    router declaration

--- week_14/auth-api/index.js
+++ week_14/auth-api/index.js
@@ -12,6 +12,24 @@ Functions and enpoints
 
 /user/note/add -> enables user to create and add notes
 
-
+user/note/delete - enables user to delete notes.
 
 */
+
+const express = require('express')
+const app = express()
+
+require('dotenv').config()
+const port = process.env.PORT
+
+//ROUTES
+
+
+//middleware
+app.use(express.json())
+
+//listening
+app.listen(port, (err) =>{
+    if (err) console.log(`Error in server setup... => ${err}`);
+    console.log(`Notes server is listening on port ${port}`)
+})
--- week_14/auth-api/router.js
+++ week_14/auth-api/router.js
@@ -1,1 +1,24 @@
 //all route definitions
+
+/*user/register -> POST REQUEST, enable users to register, validations must be put for user input
+/user/login -> POST REQUEST, user will input their username and password. 
+/user/note/read -> loads up a particular note associated with particular user
+
+/user/note/add -> enables user to create and add notes
+
+user/note/delete - enables user to delete notes.
+
+*/
+
+app.post('/register',(req, res)=>{
+
+})
+app.post('/login',(req,res)=>{})
+
+app.get('/profile',(req,res)=>{})
+
+app.post('/notes',(req,res)=>{})
+
+app.get('/notes',(req,res)=>{})
+
+app.patch('/notes/:id',(req,res)=>{})
